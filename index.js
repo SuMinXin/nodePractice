2a908cb71f9d34ef067fcaabc8846d2454224888
MinXin <sian415@gmail.com>
2020-04-05 01:04:51 +0800

    基本輸入框 &防呆

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+let onlineCount = 0;
+
 app.get('/', (request,response) => {
 	response.sendFile(__dirname + '\\view\\chatView.html');
 });
@@ -10,10 +12,22 @@ app.get('/', (request,response) => {
 io.on('connection', (socket) => {
 	console.log('Hey, Nice To See You.');
 
+	onlineCount++;
+	io.emit("online", onlineCount);
+
 	socket.on('greet', () => {
 		socket.emit("greet", "Hello, my guest.");
+		socket.emit("greet", onlineCount);
 	});
+
+	socket.on("send", (message) => {
+		console.log(message);
+	});
+
 	socket.on('disconnect', () => {
+		onlineCount = onlineCount - 1;
+		io.emit("online", onlineCount);
+
 		console.log('Bye!');
 	});
 });
